5a5acf75d923566b5c9d8480614dbb116d1a1acc
Peter <peter.walleback@gmail.com>
2016-10-27 22:24:51 +0200

    Byter nu till loginsidan när man trycker på create login

--- app/src/main/java/com/example/peter/testapp/MainActivity.java
+++ app/src/main/java/com/example/peter/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.peter.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View view) {
+        setContentView(R.layout.login_page);
     }
 }

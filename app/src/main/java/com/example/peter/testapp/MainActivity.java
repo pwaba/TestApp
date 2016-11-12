d6e65c668045f532c3a560d4e1f1a96390ea26c8
simonsays82 <simgi023@gmail.com>
2016-11-12 22:44:38 +0100

    icon

--- app/src/main/java/com/example/peter/testapp/MainActivity.java
+++ app/src/main/java/com/example/peter/testapp/MainActivity.java
@@ -1,18 +1,96 @@
 package com.example.peter.testapp;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends Activity {
+
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+        }
+
+        public void onLoginClick(View view) {
+
+            setContentView(R.layout.login_page);
+
+
+            final Button b1,b2;
+            final EditText ed1,ed2;
+
+            final TextView tx1;
+            final int counter = 3;
+
+            b1=(Button)findViewById(R.id.button);
+
+            ed1=(EditText)findViewById(R.id.editText);
+
+            ed2=(EditText)findViewById(R.id.editText2);
+
+            b2=(Button)findViewById(R.id.button2);
+            tx1=(TextView)findViewById(R.id.textView3);
+            tx1.setVisibility(View.GONE);
+
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(ed1.getText().toString().equals("admin") &&
+
+                            ed2.getText().toString().equals("admin")) {
+                        Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+
+                        tx1.setVisibility(View.VISIBLE);
+                        tx1.setBackgroundColor(Color.RED);
+                        //counter--;
+                        tx1.setText(Integer.toString(counter));
+
+                        if (counter == 0) {
+                            b1.setEnabled(false);
+                        }
+                    }
+                }
+            });
+
+            b2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+
+
+    }
     }
 
-    public void onLoginClick(View view) {
-        setContentView(R.layout.login_page);
-    }
-}
+
+
+
+
+
+
+
+
+
+
